bae212ddde11e1dae40e52de5e67b657c39de47f
Nikunj Chawla <nikchawla312@gmail.com>
2020-12-18 13:14:39 -0600

    Added GNU LGPLv3 license

--- stringOf.ml
+++ stringOf.ml
@@ -4,6 +4,21 @@
     
     {{:https://stackoverflow.com/a/65052754/14135254} Credit to Shawn} for advice on generalization with fold for the
     {{:https://tinyurl.com/ocaml-hashtbl} Hashtbl.t} and {{:https://tinyurl.com/ocaml-map-s} Map.S.t} structures
+    
+    Copyright (C) 2020 Nikunj Chawla
+    
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+    
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 *)
 
 (**
